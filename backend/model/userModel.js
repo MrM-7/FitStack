2a77c6c9408d086cd05eee0f6e8802548554885b
Mehul Raj <rmmehul7@gmail.com>
2023-05-09 11:19:52 +0530

    signup setup

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose"
+import bcrypt from 'bcrypt'
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -11,5 +12,24 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 })
+
+
+// static signup method
+userSchema.statics.signup = async function(email, password){
+
+    const exists = await this.findOne({ email })
+
+    if(exists){
+        throw Error('Email already in use')
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({ email, password: hash })
+
+    return user
+}
+
 
 export default mongoose.model('User', userSchema)
