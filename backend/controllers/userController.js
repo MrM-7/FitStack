100d7cf558b3459daa473e20f4c36dba016536a3
Mehul Raj <rmmehul7@gmail.com>
2023-05-10 23:03:27 +0530

    Signing Token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,9 @@
 import User from '../model/userModel.js'
+import jwt from 'jsonwebtoken'
+
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+}
 
 const loginUser = async (req, res) => {
     res.json({msg: 'login user'})
@@ -10,8 +15,11 @@ const signupUser = async (req, res) => {
   
     try {
       const user = await User.signup(email, password)
+
+      // create a token
+      const token = createToken(user._id)
   
-      res.status(200).json({email, user})
+      res.status(200).json({email, token})
     } catch (error) {
       res.status(400).json({error: error.message})
     }
