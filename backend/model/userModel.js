5f8e02e30bd76528082ea99eeb865f01fe6a28c5
Mehul Raj <rmmehul7@gmail.com>
2023-05-15 16:12:56 +0530

    created useLogin hook

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -2,70 +2,66 @@ import mongoose from "mongoose"
 import bcrypt from 'bcrypt'
 import validator from "validator"
 
-const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 })
 
-
 // static signup method
-userSchema.statics.signup = async function(email, password){
+userSchema.statics.signup = async function(email, password) {
 
-    // validation
-    if(!email || !password){
-        throw Error('All fields must be filled')
-    }
+  // validation
+  if (!email || !password) {
+    throw Error('All fields must be filled')
+  }
+  if (!validator.isEmail(email)) {
+    throw Error('Email not valid')
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error('Password not strong enough')
+  }
 
-    if(!validator.isEmail(email)){
-        throw Error('Email is not valid')
-    }
+  const exists = await this.findOne({ email })
 
-    if(!validator.isStrongPassword(password)){
-        throw Error('Password is not strong enough')
-    }
+  if (exists) {
+    throw Error('Email already in use')
+  }
 
-    const exists = await this.findOne({ email })
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password, salt)
 
-    if(exists){
-        throw Error('Email already in use')
-    }
+  const user = await this.create({ email, password: hash })
 
-    const salt = await bcrypt.genSalt(10)
-    const hash = await bcrypt.hash(password, salt)
-
-    const user = await this.create({ email, password: hash })
-
-    return user
+  return user
 }
 
+// static login method
+userSchema.statics.login = async function(email, password) {
 
-// static logic method
-userSchema.statics.login = async function(email, password){
+  if (!email || !password) {
+    throw Error('All fields must be filled')
+  }
 
-    if(!email || !password){
-        throw Error('All fields must be filled')
-    }
+  const user = await this.findOne({ email })
+  if (!user) {
+    throw Error('Incorrect email')
+  }
 
-    const user = await this.findOne({ email })
+  const match = await bcrypt.compare(password, user.password)
+  if (!match) {
+    throw Error('Incorrect password')
+  }
 
-    if(!user){
-        throw Error('Incorrect login credentials')
-    }
-
-    const match = await bcrypt.compare(password, user.password)
-
-    if(!match){
-        throw Error('Incorrect login credentials')
-    }
-
-    return user
+  return user
 }
 
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const Navbar = () => {
 
   const { logout } = useLogout()
+  const { user } = useAuthContext()
 
   const handleClick = () => {
     logout()
@@ -16,13 +18,15 @@ const Navbar = () => {
           <h1>FitStack</h1>
         </Link>
         <nav>
-          <div>
+          {user && (
+            <div>
+            <span>{user.email.split('@')[0]}</span>
             <button onClick={handleClick}>Log out</button>
-          </div>
-          <div>
+          </div>)}
+          {!user && (<div>
             <Link to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
-          </div>
+          </div>)}
         </nav>
       </div>
     </header>
